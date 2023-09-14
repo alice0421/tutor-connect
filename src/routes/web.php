2b05307b377ed2a090a9e6967633fd3076011ac9
有栖 <sekka.alice@gmail.com>
2023-09-14 20:54:57 +0900

    [update] config/auth.phpの設定とルーティングの実装。

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TeacherProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,11 +22,23 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/teachers/dashboard', function () {
+    return view('teachers.dashboard');
+})->middleware(['auth:teacher'])->name('teacher.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware('auth:teacher')->prefix('teachers')->name('teacher.')->group(function () {
+    Route::get('/profile', [TeacherProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [TeacherProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [TeacherProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
+
+Route::prefix('teachers')->name('teacher.')->group(function () {
+    require __DIR__.'/auth_teacher.php';
+});
