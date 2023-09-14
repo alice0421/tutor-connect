dab091ae60ffd0692a90d98cecfcf5ca0bf79b5c
有栖 <sekka.alice@gmail.com>
2023-09-14 20:59:14 +0900

    [update] マルチ認証のための修正。

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const TEACHER_HOME = '/teachers/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
