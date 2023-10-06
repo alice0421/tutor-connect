d10c7c21a1eb7bdc0bb50fd3ccb75f489f757372
有栖 <sekka.alice@gmail.com>
2023-10-06 17:22:11 +0900

    [update] 申請・講師詳細ボタンのコンポーネントを適用。

--- src/resources/views/matchings/random.blade.php
+++ src/resources/views/matchings/random.blade.php
@@ -22,10 +22,7 @@
                         <span class="w-1/3 text-sm text-gray-500">{{ $subject->name }}</span>
                     @endforeach
                 </div>
-                <div class="flex mt-4 space-x-3">
-                    <a href="" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">指導申請</a>
-                    <a href="" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200">講師詳細</a>
-                </div>
+                <x-matchings.teacher-apply-details-button :teacher="$teacher" />
             </div>
         </div>
     @endforeach
